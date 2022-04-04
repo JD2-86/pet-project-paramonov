bf38b55a04dc48c81688dfe4b123c6bfe04aba8e
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-04 20:42:59 +0300

    add: slf4j dependency

--- service/src/main/java/by/paramonov/UserDetailsServiceImpl.java
+++ service/src/main/java/by/paramonov/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package by.paramonov;
 
 import by.paramonov.dao.UserDao;
 import by.paramonov.entity.UserEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -20,8 +22,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity founduser = userDao.findByFirstName(username);
-        if (founduser == null) {
+        UserEntity foundUser = userDao.findByFirstName(username);
+        if (foundUser == null) {
             throw new UsernameNotFoundException("There no user with " + username);
         }
         return null;
