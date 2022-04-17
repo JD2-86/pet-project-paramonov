4f4391ba868c520dfe1c0aad21c8bd3fb1f49cd8
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-17 15:23:25 +0300

    modified: web/.../WebConfig

--- web/src/main/java/by/paramonov/config/WebConfig.java
+++ web/src/main/java/by/paramonov/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"by.paramonov.warehouse.controller"})
+@ComponentScan(basePackages = {"by.paramonov.warehouse"})
 
 public class WebConfig {
 }
