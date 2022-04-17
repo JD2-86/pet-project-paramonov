8a17332337bc249bd6de1d62fa462aa02d8c3342
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-17 15:58:38 +0300

    feat: @ UserEntity

--- dal/src/main/java/by/paramonov/entity/UserEntity.java
+++ dal/src/main/java/by/paramonov/entity/UserEntity.java
@@ -2,25 +2,29 @@ package by.paramonov.entity;
 
 import by.paramonov.enums.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserEntity extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstname;
+    @Column(name = "second_name")
+    private String lastname;
     @Column(name = "email")
     private String email;
     @Column(name = "username")
     private String username;
     @Column(name = "password")
     private String password;
-    @Column(name = "first_name")
-    private String firstname;
-    @Column(name = "second_name")
-    private String lastname;
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private Role role;
